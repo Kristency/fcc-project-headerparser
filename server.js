8fad367d836b350911c193bcf83782a075a4f7ed
Kristency <nandisubhajit666@gmail.com>
2020-02-03 22:25:53 +0530

    add header parser logic

--- server.js
+++ server.js
@@ -2,31 +2,34 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+let express = require('express')
+let app = express()
+let PORT = 3000
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+let cors = require('cors')
+app.use(cors({ optionSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/views/index.html')
+})
 
+// header parser API endpoint...
+app.get('/api/whoami', (req, res) => {
+	let {
+		ip: ipaddress,
+		headers: { 'accept-language': language, 'user-agent': software }
+	} = req
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
+	res.json({ ipaddress, language, software })
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.listen(PORT, () => {
+	console.log(`Your app is listening on port ${PORT}`)
+})
